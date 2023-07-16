abe766ab9d3041e467fab54c56cb4f6b6148dce6
Ajaykumar4792 <104074351+Ajaykumar4792@users.noreply.github.com>
2023-07-16 17:43:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Temp from './Components/Temp';
+function App() {
+  return (
+    <React.Fragment>
+      <Temp/>
+    </React.Fragment>
+  );
+}
+
+export default App;
